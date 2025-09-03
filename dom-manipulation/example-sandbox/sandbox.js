4544ddc2fbeb3a078d6d1cab4a48b096f6550271
Saurabh Verma <saurabh1088@gmail.com>
2025-09-03 18:50:21 +0530

    Refactoring and checks for error handling.

--- dom-manipulation/example-sandbox/sandbox.js
+++ dom-manipulation/example-sandbox/sandbox.js
@@ -1,12 +1,19 @@
 // dom-demo-enhanced.js
 
 // --- Accessing DOM Elements ---
+// Below are three ways to access an element
 const firstHeadingFromAllHeadings = document.getElementsByTagName('h1')[0];
 const headingById = document.getElementById('main-heading');
 const headingByQuerySelector = document.querySelector('h1#main-heading');
-console.log('h1 value:', firstHeadingFromAllHeadings.textContent);
-console.log('headingById value:', headingById.textContent);
-console.log('headingByQuerySelector value:', headingByQuerySelector.textContent);
+
+// If getElementById or querySelector returns null silently without any error, it means the element was not found
+// In such case below check will prevent errors
+if (firstHeadingFromAllHeadings && headingById && headingByQuerySelector) {
+    console.log('h1 value:', firstHeadingFromAllHeadings.textContent);
+    console.log('headingById value:', headingById.textContent);
+    console.log('headingByQuerySelector value:', headingByQuerySelector.textContent);
+}
+
 if (firstHeadingFromAllHeadings === headingById && headingById === headingByQuerySelector) {
     console.log('All three methods point to the same DOM element.');
 } else {
@@ -15,7 +22,10 @@ if (firstHeadingFromAllHeadings === headingById && headingById === headingByQuer
 
 // --- Modifying DOM Elements ---
 console.log('Attempting to update h1 textContent...');
-firstHeadingFromAllHeadings.textContent = 'DOM Manipulation Sandbox - Updated!';
-console.log('firstHeadingFromAllHeadings after update:', firstHeadingFromAllHeadings.textContent);
-console.log('headingById after h1 update:', headingById.textContent);
+
+if (firstHeadingFromAllHeadings && headingById && headingByQuerySelector) {
+    firstHeadingFromAllHeadings.textContent = 'DOM Manipulation Sandbox - Updated!';
+    console.log('firstHeadingFromAllHeadings after update:', firstHeadingFromAllHeadings.textContent);
+    console.log('headingById after h1 update:', headingById.textContent);
+}
 
